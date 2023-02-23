36aa8928d9876d4c7124cc220b624bb8526da074
Shirley Castro <castroshirley24@gmail.com>
2023-02-23 12:12:04 -0300

    nueva funcion consulta vencimientos cercanos

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ class Producto{
 
 //CREAMOS PRODUCTOS
 
-const snackPalitosPanceta = new Producto (8858223012377, "Snack palitos sabor panceta ahumada 60 gr", 20230913, 3, "Snacks");
-const snackPalitosPicante = new Producto (8858283008479, "Snack palitos sabor picante 60 gr", 20230913, 3, "Snacks");
-const caramelosBerry = new Producto (4710035230230, "Caramelos masticables Hi-Chew sabor frutos rojos 110 gr", 20230601, 3, "Golosinas");
-const lataCaramelos = new Producto (4901630002531, "Lata de caramelos Tumba de Luciérnagas Sakura Drops", 20240911, 3, "Golosinas");
+const snackPalitosPanceta = new Producto (8858223012377, "Snack palitos sabor panceta ahumada 60 gr", "2023-05-13", 3, "Snacks");
+const snackPalitosPicante = new Producto (8858283008479, "Snack palitos sabor picante 60 gr", "2023-04-13", 3, "Snacks");
+const caramelosBerry = new Producto (4710035230230, "Caramelos masticables Hi-Chew sabor frutos rojos 110 gr", "2023-04-01", 3, "Golosinas");
+const lataCaramelos = new Producto (4901630002531, "Lata de caramelos Tumba de Luciérnagas Sakura Drops", "2024-04-24", 3, "Golosinas");
 
 // CREAMOS UN ARRAY CON LOS PRODUCTOS 
 
@@ -29,7 +29,7 @@ const arrayProductosSinStock = [];
 //MENÚ
 
 function menu(){
-    let opcion = parseInt(prompt("Ingrese una opción: 1)Agregar productos nuevos 2)Modificar stock 3)Consultar producto 4)Eliminar producto 5)Salir"));
+    let opcion = parseInt(prompt("Ingrese una opción: 1)Agregar productos nuevos 2)Modificar stock 3)Consultar producto 4)Eliminar producto 5)Vencimientos cercanos 6)Salir"));
     return opcion;
 };
 
@@ -38,7 +38,7 @@ function menu(){
 function agregarProductos(){
     let plu = prompt("Ingrese el código plu del producto");
     let nombre = prompt("Ingrese el nombre del producto");
-    let fecha = prompt("Ingrese la fecha de vencimiento del producto (aaaammdd)");
+    let fecha = prompt("Ingrese la fecha de vencimiento del producto (aaaa-mm-dd)");
     let cantidad = prompt("Ingrese el stock disponible del producto");
     let categoria = prompt("Ingrese la categoria del producto");
     const producto = new Producto (plu, nombre, fecha, cantidad, categoria);
@@ -84,6 +84,22 @@ function eliminarProducto(){
     console.log(arrayProductos);
 };
 
+//CONSULTAR VENCIMIENTOS CERCANOS
+function vencimiento(){
+   const hoy = new Date();
+   const dosMesesDespues = new Date(hoy.getFullYear(), hoy.getMonth()+2, hoy.getDate());
+   const vencimientosCercanos = arrayProductos.filter(producto =>{
+    const fechaVencimiento = new Date(producto.fecha);
+    return fechaVencimiento > hoy && fechaVencimiento <= dosMesesDespues;
+   })
+   if(vencimientosCercanos.length > 0){
+    alert(`Tienes productos próximos a vencer`);
+    console.log(vencimientosCercanos);
+   }else{
+    alert(`No tienes productos próximos a vencer`);
+   }
+};
+
 //SALIR
 
 function salir(){
@@ -103,7 +119,9 @@ switch (opcion){
     break;
     case 4: eliminarProducto();
     break;
-    case 5: salir();
+    case 5: vencimiento();
+    break;
+    case 6: salir();
     break;
     default: 
     alert(`Opción no encontrada`);
